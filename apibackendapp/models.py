2785925210bf0def5ebca5b654a46ca8e4c6cb65
rakesh0327-coder <rakeshvasudevan0327@gmail.com>
2024-11-13 16:55:17 +0530

    my second version

--- apibackendapp/models.py
+++ apibackendapp/models.py
@@ -3,10 +3,12 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save
 from django.conf import settings
 from rest_framework.authtoken.models import Token
+
+
     
 
 
-# Create your models here.
+# Create your beautiful models here.
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 
